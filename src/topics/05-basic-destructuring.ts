a3fd2536a57d3cc74305f1077772d309700cc3c7
Jaime Alonso Alvarez <jaimealonso.alvarez1984@gmail.com>
2026-03-17 23:07:57 -0500

    basic destructuring

---
+++ src/topics/05-basic-destructuring.ts
@@ -0,0 +1,33 @@
+interface AudioPlayer {
+    audioVolume: number;
+    songDuration: number;
+    song: string;
+    details: Details;
+}
+
+interface Details {
+    author: string;
+    year: number
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolume: 50, 
+    songDuration: 36, 
+    song: "Highs And Lows", 
+    details: {
+        author: 'Hillsong', 
+        year: 2021
+    }
+}
+
+const { song:anotherSong, details } = audioPlayer;
+
+const { author, year, } = details
+
+
+console.log(`Song: ${ anotherSong }`)
+console.log(`Details: ${ author } ${ year }`)
+
+
+
+export {}
