bbeb3f62d7cafd81e6066b87598b036a72713935
Jaime Alonso Alvarez <jaimealonso.alvarez1984@gmail.com>
2026-03-17 22:04:32 -0500

    - Homework for types.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 // import './topics/01-basic-types';
 // import './topics/02-object-interface';
-import './topics/03-functions';
+// import './topics/03-functions';
+import './topics/04-homework-types'
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
