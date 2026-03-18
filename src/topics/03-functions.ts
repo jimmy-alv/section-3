2d0e159b96c3ac608d9cf58323e45b6ebfcd188a
Jaime Alonso Alvarez <jaimealonso.alvarez1984@gmail.com>
2026-03-17 21:56:24 -0500

    - Advanced functions in interfaces class.

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -1,21 +1,44 @@
-function addNumbers(a: number, b: number): number {
-    return a + b;
+// function addNumbers(a: number, b: number): number {
+//     return a + b;
+// }
+// const result = addNumbers(1, 2)
+// console.log({ result })
+
+// const addNumbersArrow = (a: number, b: number): string => {
+//     return `${ a + b }`;
+// }
+// const result2 = addNumbersArrow(2, 3);
+// console.log(result2);
+
+// function multiply(firstNumber: number, secondNumber?: number, base: number = 2): number {
+//     return firstNumber * base;
+// }
+// const resultMultiply = multiply(3)
+
+// console.log({ result, result2, resultMultiply })
+
+interface Character { 
+    name:string;
+    hp: number;
+    showHp: () => void;
 }
-const result = addNumbers(1, 2)
-console.log({ result })
 
-const addNumbersArrow = (a: number, b: number): string => {
-    return `${ a + b }`;
+
+const healCharacter = ( character: Character, amount: number ) => {
+    character.hp += amount;
 }
-const result2 = addNumbersArrow(2, 3);
-console.log(result2);
 
-function multiply(firstNumber: number, secondNumber?: number, base: number = 2): number {
-    return firstNumber * base;
+const strider: Character = {
+    name: 'Aragon', 
+    hp: 50, 
+    showHp() {
+        console.log(`Health points ${ this.hp }`)
+    }
 }
-const resultMultiply = multiply(3)
 
-console.log({ result, result2, resultMultiply })
+healCharacter(strider, 10);
+healCharacter(strider, 40);
 
+strider.showHp();
 
 export {};
